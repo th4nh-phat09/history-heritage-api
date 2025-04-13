8e7a0af7e5b9713e93e22d1a26c763278913b93b
phanvanthuan06052004 <phanthuan652004@gmail.com>
2025-04-14 00:22:22 +0700

    handle user model

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,1 +1,16 @@
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { userRoute } from './userRoute'
 
+const Router = express.Router()
+
+// check status v1
+Router.get('/status', (req, res) => {
+  res.status(StatusCodes.OK).json({ message: 'API v1 is working' })
+})
+
+// user APIs
+Router.use('/users', userRoute)
+
+
+export const APIs_V1 = Router
