889f8054481494ab93b60d074d070a4f1fc9a6e6
zerrorTwo <namle022004@gmail.com>
2025-04-15 21:35:04 +0700

    sigin

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,19 +1,18 @@
-import express from 'express'
-import { StatusCodes } from 'http-status-codes'
-import { historyHeritageRoute } from './historyHeritageRoute'
-import { userRoute } from './userRoute'
-const Router = express.Router()
+import express from "express";
+import { StatusCodes } from "http-status-codes";
+import { historyHeritageRoute } from "./historyHeritageRoute";
+import { userRoute } from "./userRoute";
+const Router = express.Router();
 
 // check status v1
-Router.get('/status', (req, res) => {
-    res.status(StatusCodes.OK).json({ message: 'API v1 is working' })
-})
+Router.get("/status", (req, res) => {
+  res.status(StatusCodes.OK).json({ message: "API v1 is working" });
+});
 
 // historyHeritageRoute APIs
-Router.use('/heritages', historyHeritageRoute)
+Router.use("/heritages", historyHeritageRoute);
 
 // user APIs
-Router.use('/users', userRoute)
+Router.use("/users", userRoute);
 
-export const APIs_V1 = Router
-
+export const APIs_V1 = Router;
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,8 +1,8 @@
-import { StatusCodes } from 'http-status-codes'
-import { userModel } from '~/models/userModel'
-import ApiError from '~/utils/ApiError'
-import bcryptjs from 'bcryptjs'
-import { v4 as uuidv4 } from 'uuid'
+import { StatusCodes } from "http-status-codes";
+import { userModel } from "~/models/userModel";
+import ApiError from "~/utils/ApiError";
+import bcryptjs from "bcryptjs";
+import { v4 as uuidv4 } from "uuid";
 
 const getAll = async () => {
   // try {
@@ -13,37 +13,67 @@ const getAll = async () => {
   // } catch (error) {
   //   throw error
   // }
-}
+};
 
-const createNew = async ( reqBody ) => {
+const createNew = async (reqBody) => {
   try {
     // check email có tồn tại hay không
-    const checkEmail = await userModel.findOneByEmail(reqBody.email)
+    const checkEmail = await userModel.findOneByEmail(reqBody.email);
     if (checkEmail) {
-      throw new ApiError(StatusCodes.CONFLICT, 'Email already exited!')
+      throw new ApiError(StatusCodes.CONFLICT, "Email already exited!");
     }
     // khởi tạo data
-    const nameFromEmail = reqBody.email.split('@')[0]
+    const nameFromEmail = reqBody.email.split("@")[0];
     const newUser = {
       email: reqBody.email,
       password: bcryptjs.hashSync(reqBody.password, 8),
       username: nameFromEmail,
       displayname: nameFromEmail,
-      verifyToken: uuidv4()
-    }
+      verifyToken: uuidv4(),
+    };
     // lưu data
-    const result = await userModel.createNew(newUser)
-    const getNewUser = await userModel.findOneById(result.insertedId)
+    const result = await userModel.createNew(newUser);
+    const getNewUser = await userModel.findOneById(result.insertedId);
     // verify  email
     // retrun data
-    return getNewUser
+    return getNewUser;
+  } catch (error) {
+    throw error;
   }
-  catch (error) {
-    throw error
+};
+
+const signIn = async (reqBody) => {
+  try {
+    // check email có tồn tại hay không
+    const user = await userModel.findOneByEmail(reqBody.email);
+    if (!user) {
+      throw new ApiError(StatusCodes.NOT_FOUND, "User not found!");
+    }
+
+    const isValidPassword = await bcryptjs.compare(
+      reqBody.password,
+      user.password
+    );
+    if (!isValidPassword) {
+      throw new ApiError(
+        StatusCodes.UNAUTHORIZED,
+        "Invalid email or password!"
+      );
+    }
+    if (user?.isVerified === false) {
+      throw new ApiError(StatusCodes.UNAUTHORIZED, "Email not verified");
+    }
+
+    const { password: _, ...userWithoutPassword } = user;
+
+    // retrun data
+    return userWithoutPassword;
+  } catch (error) {
+    throw error;
   }
-}
+};
 
 export const userService = {
   getAll,
-  createNew
-}
+  createNew,
+};
