3db22d689813c13043260a32449794df50d683c6
zerrorTwo <namle022004@gmail.com>
2025-04-26 14:20:43 +0700

    upload avatar

---
+++ src/routes/v1/uploadRoute.js
@@ -0,0 +1,83 @@
+import express from 'express'
+import path from 'path'
+import multer from 'multer'
+import fs from 'fs'
+
+const Router = express.Router()
+
+const __dirname = path.resolve()
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, 'uploads/avatar'))
+  },
+
+  filename: (req, file, cb) => {
+    const extname = path.extname(file.originalname)
+    cb(null, `${file.fieldname}-${Date.now()}${extname}`)
+  }
+})
+
+const fileFilter = (req, file, cb) => {
+  const filetypes = /jpe?g|png|webp|gif|bmp|tiff/
+  const mimetypes =
+        /image\/jpe?g|image\/png|image\/webp|image\/gif|image\/bmp|image\/tiff/
+
+  const extname = path.extname(file.originalname).toLowerCase()
+  const mimetype = file.mimetype
+
+  if (filetypes.test(extname) && mimetypes.test(mimetype)) {
+    cb(null, true)
+  } else {
+    cb(new Error('Images only'), false)
+  }
+}
+
+const upload = multer({ storage, fileFilter })
+const uploadSingleImage = upload.single('image')
+
+Router.post('/', (req, res) => {
+  uploadSingleImage(req, res, (err) => {
+    if (err) {
+      return res.status(400).send({ message: err.message })
+    }
+
+    if (req.file) {
+      // Construct the full image path to be returned to the client
+      const imagePath = `/uploads/avatar/${req.file.filename}`
+      return res.status(200).send({
+        message: 'Image uploaded successfully',
+        image: imagePath // Return the accessible path
+      })
+    } else {
+      return res.status(400).send({ message: 'No image file provided' })
+    }
+  })
+})
+
+Router.delete('/', (req, res) => {
+  const { image } = req.body
+
+  if (!image) {
+    return res.status(400).send({ message: 'Filename is required' })
+  }
+
+  // Construct the full file path on the server
+  const filePath = path.join(__dirname, image)
+
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      return res.status(404).send({ message: 'File not found' })
+    }
+
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        console.error('Error deleting file:', err)
+        return res.status(500).send({ message: 'Failed to delete image' })
+      }
+      return res.status(200).send({ message: 'Image deleted successfully' })
+    })
+  })
+})
+
+export const uploadRoute = Router
